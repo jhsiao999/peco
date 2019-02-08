6fdf6caed9493420a3677d53b9c1086f63707e26
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-02-08 16:33:41 -0600

    Fixed up roxygen2 docs for data sets.

--- R/data.R
+++ R/data.R
@@ -1,41 +1,69 @@
 #' @name eset_final_sub
+#'
+#' @docType data
 #' 
-#' Filtered gene expression data, along with summarized FUCCI intensities
-#' and sample information. Contain the top 100 cyclic genes from our data.
+#' @title Filtered gene expression data
+#' 
+#' @description Filtered gene expression data, along with summarized FUCCI
+#' intensities and sample information. Contains the top 100 cyclic
+#' genes from our data.
 #'
 #'
-#' @format An ExpressionSet object with 888 samples and top 100 cyclicl genes.
-#'     \describe{
-#'        \item{\code{theta}}{Inferred angles}
-#'        \item{\code{exprs(est_final)}}{Molecule counts}
-#'      }
+#' @format An ExpressionSet object with 888 samples and top 100 cyclic
+#' genes.
+#' 
+#' \describe{
+#'   \item{theta}{Inferred angles.}
+#' 
+#'   \item{\code{exprs(est_final)}}{Molecule counts.}
+#' }
+#'
+#' @keywords data
+#' 
 NULL
 
 #' @name genes_cyclic_list
 #'
-#' Genes ordered by their cyclic trend (stronog to weak)
-#' and sample information
+#' @docType data
+#' 
+#' @title Genes ordered by their cyclic trend
 #'
+#' @description Genes ordered by their cyclic trend (strong to weak),
+#' and sample information.
 #'
-#' @format A data frame
-#'     \describe{
-#'        \item{\code{order}}{The ordering of the genes according to proportion of
-#'             variacne explained (large to small)}
-#'        \item{\code{ensg}}{ENSG gene ID}
-#'        \item{\code{pve}}{Proportion of variance explained in the expression values by the
-#'             estimated cyclic trend}
-#'      }
+#' @format A data frame with the following columns, in which the rows
+#' are ordered according to the proportion of variance explained
+#' (large to small):
+#' 
+#' \describe{
+#' 
+#'   \item{ensg}{ENSG gene ID.}
+#' 
+#'   \item{pve}{Proportion of variance explained in the expression
+#'   values by the estimated cyclic trend.}
+#' }
+#'
+#' @keywords data
+#' 
 NULL
 
 #' @name fit
 #'
-#' Pre-computed results, including training and prediction
-#' and sample information
+#' @docType data
+#' 
+#' @title Training and prediction results
+#' 
+#' @description Pre-computed results, including training and
+#' prediction and sample information.
 #'
+#' @format
+#' 
+#' \describe{
+#'   \item{\code{fit_train}}{training results}
+#' 
+#'   \item{\code{fit_predict}}{prediction results}
+#'  }
 #'
-#' @format Two objects
-#'     \describe{
-#'        \item{\code{fit_train}}{training results}
-#'        \item{\code{fit_predict}}{prediction results}
-#'      }
+#' @keywords data
+#' 
 NULL
