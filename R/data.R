6f5383dea04243a732b0f5d1ef512a7504716d2b
Joyce Hsiao <joyce.hsiao1@gmail.com>
2019-02-19 11:49:20 -0600

    minor edits

--- R/data.R
+++ R/data.R
@@ -1,69 +1,90 @@
 #' @name eset_final_sub
 #'
 #' @docType data
-#' 
-#' @title Filtered gene expression data
-#' 
-#' @description Filtered gene expression data, along with summarized FUCCI
-#' intensities and sample information. Contains the top 100 cyclic
-#' genes from our data.
 #'
+#' @title Molecule counts of top 100 cyclical genes in 888 samples analyzed
+#'   in the study.
 #'
-#' @format An ExpressionSet object with 888 samples and top 100 cyclic
-#' genes.
-#' 
+#' @description An ExpressionSet object (require Biobase package) including
+#'   molecule count data after gene and smaple filtering. The phenotypeData() slot
+#'   contains sample phenotype information and the featureData() slot contains
+#'   gene feature information.
+#'
+#' @format An ExpressionSet object with 888 samples and top 100 cyclic genes.
+#'
 #' \describe{
-#'   \item{theta}{Inferred angles.}
-#' 
-#'   \item{\code{exprs(est_final)}}{Molecule counts.}
+#'   \item{\code(pData(eset_final_sub)$theta}{Inferred angles of each cell along
+#'     a circle, also known as FUCCI phase - cell cycle phase inferred from
+#'     FUCCI intensities.}
+#'
+#'   \item{\code{exprs(est_final_sub)}}{Molecule counts of top 100 cyclical genes.}
 #' }
 #'
 #' @keywords data
-#' 
+#'
 NULL
 
 #' @name genes_cyclic_list
 #'
 #' @docType data
-#' 
-#' @title Genes ordered by their cyclic trend
 #'
-#' @description Genes ordered by their cyclic trend (strong to weak),
-#' and sample information.
+#' @title List of top 100 cyclic genes
+#'
+#' @description Top 100 cyclic genes ordered by their cyclic trend (strong to weak)
+#'   across 888 samples analyzed in the study.
 #'
 #' @format A data frame with the following columns, in which the rows
 #' are ordered according to the proportion of variance explained
 #' (large to small):
-#' 
+#'
 #' \describe{
-#' 
+#'
 #'   \item{ensg}{ENSG gene ID.}
-#' 
+#'
 #'   \item{pve}{Proportion of variance explained in the expression
 #'   values by the estimated cyclic trend.}
 #' }
 #'
 #' @keywords data
-#' 
+#'
 NULL
 
-#' @name fit
+#' @name fit_train
 #'
 #' @docType data
-#' 
-#' @title Training and prediction results
-#' 
-#' @description Pre-computed results, including training and
-#' prediction and sample information.
+#'
+#' @title Traing model results among samples from 5 individuals.
+#'
+#' @description Pre-computed results. Applied \code{cycle_npreg_insample} to
+#'   obtain gene-specific cyclic trend parameters using samples from 5
+#'   individuals
 #'
 #' @format
-#' 
+#'
 #' \describe{
 #'   \item{\code{fit_train}}{training results}
-#' 
-#'   \item{\code{fit_predict}}{prediction results}
+#'
+#' @keywords data
+#'
+NULL
+
+#' @name fit_pred
+#'
+#' @docType data
+#'
+#' @title Results of predicting cell cycle phase for samples from NA19098.
+#'
+#' @description Pre-computed results. Applied \code{cycle_npreg_outsample} and
+#'   results stored in \code{fit_train} to predict cell cycle phase for
+#'   single-cell samples of NA19098.
+#'
+#' @format
+#'
+#' \describe{
+#'   \item{\code{Y_reordered}}{Normalized expression values (log2CPM , )}
+#'   \item{\code{cell_times_reordered}}{Estimated phase}
 #'  }
 #'
 #' @keywords data
-#' 
+#'
 NULL
